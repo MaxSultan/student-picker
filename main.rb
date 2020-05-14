9f76f2fcbcd174bf749f616b75bb7cb075bcc66d
Max Sultan <maxsultakent@gmail.com>
2020-05-14 11:08:19 -0600

    stubbed out comments for project

---
+++ main.rb
@@ -0,0 +1,13 @@
+students = ["Leveille, Andre", "AshWorth, Ryan", "Biddinger, Caden", "Mbia, Arsene", 
+"Boyden, Lindsay", "DelaCruz, Joey", "Dunshee, Kyle", "Riches, James", "Smith, Shawn", 
+"Sultan, Maxwell", "Zarate, Javier"]
+
+# create a group randomizer
+
+#create a new array of hashes with keys first_name and last_name
+
+# with the new array randomly select people and assign into groups
+
+#print out groups in a slack friendly format
+
+student.map ||
